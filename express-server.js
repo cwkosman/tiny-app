606881a2d6792966ee4ebb9c6aeef1addede9303
cwkosman <cwkosman@gmail.com>
2017-02-14 22:10:01 +0000

    Prepare for ejs render
    Added render function connected to ejs file

--- express-server.js
+++ express-server.js
@@ -1,11 +1,14 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -13,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
